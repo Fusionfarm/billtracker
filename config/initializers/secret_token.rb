36b56806d9e9571ff3306545680ac194231c981e
aaronf <afrerich@gmail.com>
2012-12-14 19:44:50 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billtracker::Application.config.secret_token = 'd02a7d1284ba6975953c2ffed42b29b5a498dba0d711fa832b11585f2278966e004e34e4bf58111aac9b5562c5330fe74d1f60a2240931eb89c4c5dcfc95996d'
